38a5fadf9f0972e58ae5a0105e5f7f83e1bb9fdb
Jamaican-Boy <dime8582@gmail.com>
2024-05-24 00:48:41 +0530

    socket

--- client/src/pages/Chat/index.js
+++ client/src/pages/Chat/index.js
@@ -5,7 +5,7 @@ import UserSearch from "./components/UserSearch";
 import UsersList from "./components/UsersList";
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:5000');
+const socket = io("https://work-management-tracker-chat.onrender.com");
 function Chat() {
   const [searchKey, setSearchKey] = React.useState("");
   const { selectedChat, user } = useSelector((state) => state.users);
